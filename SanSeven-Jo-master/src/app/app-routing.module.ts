e57c3db603f787576b909ddd2de22a1089375ca5
JoRPM <44869196+JoRPM@users.noreply.github.com>
2019-07-18 12:00:58 -0300

    update

--- SanSeven-Jo-master/src/app/app-routing.module.ts
+++ SanSeven-Jo-master/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "**", component: PagenotfoundComponent}
 ];
 
 @NgModule({
